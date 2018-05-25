e99c3ba0e779fb615ed24c2622cacb18a4c87129
Denis Haine <denis.haine@gmail.com>
2018-05-25 10:55:26 -0400

    Fix comment in data load.

--- data_load.R
+++ data_load.R
@@ -16,6 +16,5 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 library(data.table)
-#library(lubridate)
 NEI <- as.data.table(NEI)
 SCC <- as.data.table(SCC)
