bd1468fc2bfd895340389b21aa3f6f8a028564a5
Denis Haine <denis.haine@gmail.com>
2018-05-25 09:53:39 -0400

    Data load.

---
+++ data_load.R
@@ -0,0 +1,21 @@
+## Downloading data if necessary.
+
+filename <- "exdata_data_NEI_Data.zip"
+
+if (!file.exists(filename)){
+  fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  download.file(fileurl, filename)
+}  
+if (!file.exists("exdata_data_NEI_Data")) { 
+  unzip(filename) 
+}
+
+## Reading data
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+library(data.table)
+#library(lubridate)
+NEI <- as.data.table(NEI)
+SCC <- as.data.table(SCC)
